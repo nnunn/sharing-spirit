df7cac108c87f9ef463ba6556f982c19c3d5c898
Natasha Nunn <natasha@silversky.ca>
2015-02-11 11:10:10 -0800

    changing database

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150209172059) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "videos", force: true do |t|
     t.string   "title"
     t.string   "vimeoid"
